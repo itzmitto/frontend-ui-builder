b463c9c307fb5bc5597af7a178129a6744829231
itzmitto <andre.babirian2004@gmail.com>
2026-08-01 18:35:53 +0200

    home pagina bijgewerkt

---
+++ frontend-ui-builder/app/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/components/home/Hero";
+
+export default function HomePage() {
+    return (
+        <main className="bg-[#09090b]">
+            <Hero />
+        </main>
+    );
+}
