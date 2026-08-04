c22636f1c891508574b394f7fc224c7f62fe308c
itzmitto <andre.babirian2004@gmail.com>
2026-08-04 18:42:14 +0200

    add EditorProperties component to EditorMockup for enhanced functionality

--- frontend-ui-builder/components/home/EditorMockup.tsx
+++ frontend-ui-builder/components/home/EditorMockup.tsx
@@ -2,6 +2,7 @@
 
 import EditorWindow from "./editor-mockup/EditorWindow";
 import EditorSidebar from "./editor-mockup/EditorSidebar";
+import EditorProperties from "./editor-mockup/EditorProperties";
 
 export default function EditorMockup() {
     return (
@@ -14,13 +15,15 @@ export default function EditorMockup() {
 
                         <EditorSidebar />
 
-                        <div className="flex flex-1 items-center justify-center">
+                        <div className="flex flex-1 items-center justify-center border-x border-zinc-800 bg-zinc-950/30">
 
                             <h2 className="text-4xl font-bold text-zinc-700">
                                 Live Preview
                             </h2>
 
                         </div>
+
+                        <EditorProperties />
 
                     </div>
 
