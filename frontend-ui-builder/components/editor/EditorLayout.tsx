16afe5e56e604e92327366a1bd83b4a3765f5376
itzmitto <andre.babirian2004@gmail.com>
2026-08-05 21:04:53 +0200

    refactor EditorLayout to improve structure and organization of AssetsPanel and LayersPanel

--- frontend-ui-builder/components/editor/EditorLayout.tsx
+++ frontend-ui-builder/components/editor/EditorLayout.tsx
@@ -3,31 +3,30 @@
 import TemplateSidebar from "./TemplateSidebar";
 import PropertyPanel from "./PropertyPanel";
 import Toolbar from "./Toolbar";
-import TemplateRenderer from "@/components/templates/renderer/TemplateRenderer";
+import AssetsPanel from "./AssetsPanel";
 import LayersPanel from "./LayersPanel";
+
+import TemplateRenderer from "@/components/templates/renderer/TemplateRenderer";
 
 export default function EditorLayout() {
     return (
         <div className="flex h-[calc(100vh-64px)]">
             <TemplateSidebar />
-
             <main className="flex flex-1 flex-col">
                 <Toolbar />
-
                 <TemplateRenderer />
             </main>
-
-            <div className="flex w-96 flex-col border-l border-zinc-800 bg-zinc-950">
-
+            <aside className="flex w-96 flex-col border-l border-zinc-800 bg-zinc-950">
                 <div className="flex-1 overflow-y-auto">
                     <PropertyPanel />
                 </div>
-
                 <div className="border-t border-zinc-800 p-4">
-                    <LayersPanel />
+                    <div className="space-y-4">
+                        <AssetsPanel />
+                        <LayersPanel />
+                    </div>
                 </div>
-
-            </div>
+            </aside>
         </div>
     );
 }
